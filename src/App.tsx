fb6278016c21dc227d85caea5cf8f6944d971830
Victor <emmyvic98@gmail.com>
2023-06-08 10:03:30 +0100

    refactored with custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import { styled } from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationPin } from "@fortawesome/free-solid-svg-icons";
+import { useArrayToFill } from "./hooks/useArrayToFill";
 
 interface InnerBoxProperties {
   rotate: number;
@@ -12,22 +13,8 @@ interface InnerBoxProperties {
 }
 
 export const App = () => {
-  const arrayToFill = [];
-  const colorIndex = [
-    48, 2, 3, 2, 6, 2, 3, 2, 3, 2, 6, 2, 6, 2, 3, 2, 3, 2, 3, 2, 6, 2, 3, 2, 3, 2,
-    6, 2, 6, 2, 3, 2, 3, 2, 3, 2, 6, 2, 3, 2, 3, 2, 3, 2, 6, 2, 3, 2,
-  ];
+  const arrayToFill = useArrayToFill();
   let rotate = 0;
-  // 2 is slateblue
-  // 3 is dogerblue
-  // 6 is darkkhaki
-  // 48 is salmon
-  for (let index = 0; index <= colorIndex.length; index++) {
-    if (colorIndex[index] === 48) arrayToFill.push("salmon");
-    if (colorIndex[index] === 6) arrayToFill.push("darkkhaki");
-    if (colorIndex[index] === 3) arrayToFill.push("dodgerblue");
-    if (colorIndex[index] === 2) arrayToFill.push("slateblue");
-  }
   return (
     <Container>
       <Pointer>
